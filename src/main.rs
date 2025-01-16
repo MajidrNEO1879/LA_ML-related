4255a0e0a4f324825b32578de265e3f6dd299c4e
MajidrNEO1879 <majidrezaeiphy1879@gmail.com>
2025-01-16 18:54:51 +0000

    testing done. the same link

--- src/main.rs
+++ src/main.rs
@@ -88,6 +88,33 @@ fn plot_data(
 }
 
 
+//training the model 
+fn iterate_with_values(
+    train: &DatasetBase<
+        ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>,
+        ArrayBase<OwnedRepr<&'static str>, Dim<[usize; 2]>>,
+    >,
+    test: &DatasetBase<
+        ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>,
+        ArrayBase<OwnedRepr<&'static str>, Dim<[usize; 2]>>,
+    >,
+    threshold: f64,
+    max_iterations: u64,
+) -> ConfusionMatrix<&'static str> {
+    let model = LogisticRegression::default()
+        .max_iterations(max_iterations)
+        .gradient_tolerance(0.0001)
+        .fit(train)
+        .expect("can train model");
+
+    let validation = model.set_threshold(threshold).predict(test);
+
+    let confusion_matrix = validation
+        .confusion_matrix(test)
+        .expect("can create confusion matrix");
+
+    confusion_matrix
+}
 fn main()
 {
     let train = load_data("./dataset_test_1.csv");
@@ -106,4 +133,38 @@ fn main()
 
     println!("plotting data...");
     plot_data(&train);
+
+
+    //
+    println!("training and testing model...");
+    let mut max_accuracy_confusion_matrix = iterate_with_values(&train, &test, 0.01, 100);
+    let mut best_threshold = 0.0;
+    let mut best_max_iterations = 0;
+    let mut threshold = 0.02;
+
+    for max_iterations in (1000..5000).step_by(500) {
+        while threshold < 1.0 {
+            let confusion_matrix = iterate_with_values(&train, &test, threshold, max_iterations);
+
+            if confusion_matrix.accuracy() > max_accuracy_confusion_matrix.accuracy() {
+                max_accuracy_confusion_matrix = confusion_matrix;
+                best_threshold = threshold;
+                best_max_iterations = max_iterations;
+            }
+            threshold += 0.01;
+        }
+        threshold = 0.02;
+    }
+
+    println!(
+        "most accurate confusion matrix: {:?}",
+        max_accuracy_confusion_matrix
+    );
+    println!(
+        "with max_iterations: {}, threshold: {}",
+        best_max_iterations, best_threshold
+    );
+    println!("accuracy {}", max_accuracy_confusion_matrix.accuracy(),);
+    println!("precision {}", max_accuracy_confusion_matrix.precision(),);
+    println!("recall {}", max_accuracy_confusion_matrix.recall(),);
 }
